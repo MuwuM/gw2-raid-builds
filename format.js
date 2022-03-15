dd333e7dfd743f188f4a005de2eac1cefa49fe82
Lukas Theis <muwum@muwum.net>
2022-03-16 00:49:15 +0100

    apply default formatting add initial set of boons

---
+++ format.js
@@ -0,0 +1,36 @@
+const fs = require("fs-extra");
+const path = require("path");
+
+(async() => {
+  const buildsFile = path.resolve(process.cwd(), "builds.json");
+  const builds = await fs.readJSON(buildsFile);
+
+  const formattedBuilds = builds.map((build) => {
+    const formatted = ({
+      class: build.class,
+      spec: build.spec,
+      role: build.role,
+      name: build.name,
+      boons: {
+        alac: (build.boons && typeof build.boons.alac === "number") ? build.boons.alac : false,
+        quick: (build.boons && typeof build.boons.quick === "number") ? build.boons.quick : false,
+        might: (build.boons && typeof build.boons.might === "number") ? build.boons.might : false,
+        fury: (build.boons && typeof build.boons.fury === "number") ? build.boons.fury : false
+      },
+      gw2skills: build.gw2skills || false,
+      snowcrows: build.snowcrows || false,
+      luckynoobs: build.luckynoobs || false,
+      bench: typeof build.bench === "number" ? build.bench : false,
+      benchWithAllies: typeof build.benchWithAllies === "number" ? build.benchWithAllies : false,
+      benchLarge: typeof build.benchLarge === "number" ? build.benchLarge : false,
+      benchConfusion: typeof build.benchConfusion === "number" ? build.benchConfusion : false,
+      benchOld: build.benchOld || false
+    });
+    return formatted;
+  });
+  await fs.outputJSON(buildsFile, formattedBuilds, {spaces: 2});
+
+})().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
